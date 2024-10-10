20fbf8b07e9aa4bd0c8457a794e49f07f9cdedf3
cmabdulkareem <cmabdulkareem@gmail.com>
2024-10-10 15:45:54 +0530

    app.js

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const app =express()
 const PORT =process.env.PORT || 3000
 
 const corsOptions = {
-      origin:"http://localhost:5173",
+      origin:"https://perfume-black.vercel.app/",
       method: "GET,POST,HEAD,PUT,PATCH,DELETE",
       credentials: true,
       allowedHeader: "Content-Type, Authorization"
