73cf8e9b784c81da6cd28a79ab10f7dd9f73a484
cmabdulkareem <cmabdulkareem@gmail.com>
2024-10-11 14:08:41 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,56 +1,58 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import './config/db.js'
-import cors from 'cors'
-import session from 'express-session'
-import cookieparser from 'cookie-parser'
-import MongoStore from 'connect-mongo'
-import fileUpload from 'express-fileupload'
+import express from 'express';
+import dotenv from 'dotenv';
+import './config/db.js';
+import cors from 'cors';
+import session from 'express-session';
+import cookieparser from 'cookie-parser';
+import MongoStore from 'connect-mongo';
+import fileUpload from 'express-fileupload';
 
-import userRouter from './routes/userRoutes.js'
+import userRouter from './routes/userRoutes.js';
 
-dotenv.config()
+dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const app =express()
-const PORT =process.env.PORT || 3000
+// Check if the app is running in production and set the trust proxy setting
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1);
+}
 
 const sessionStorage = MongoStore.create({
-     mongoUrl: 'mongodb+srv://kareem:kar123@sample.kvxwkea.mongodb.net',
-     dbName: 'perfume',
-     collectionName: 'sessions',
-     ttl: 24*60*60,
-     autoRemove : 'native'
-})
-const corsOptions = {
-     origin:"https://perfume-black.vercel.app",
-      methods: "GET,POST,HEAD,PUT,PATCH,DELETE",
-      credentials: true,
-      allowedHeader: "Content-Type, Authorization"
+    mongoUrl: 'mongodb+srv://kareem:kar123@sample.kvxwkea.mongodb.net',
+    dbName: 'perfume',
+    collectionName: 'sessions',
+    ttl: 24 * 60 * 60,
+    autoRemove: 'native'
+});
 
-}
-app.use(cors(corsOptions))
-app.use(express.static("public"))
-app.use(fileUpload())
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-app.use(cookieparser())
+const corsOptions = {
+    origin: "https://perfume-black.vercel.app",
+    methods: "GET,POST,HEAD,PUT,PATCH,DELETE",
+    credentials: true,
+    allowedHeader: "Content-Type, Authorization"
+};
+
+app.use(cors(corsOptions));
+app.use(express.static("public"));
+app.use(fileUpload());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieparser());
 app.use(session({
-  secret: "secret",
-  resave: false,
-  saveUninitialized: true,
-  store: sessionStorage,
-  cookie: {
-    secure: true, 
-    maxAge: 100000 
-  }
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStorage,
+    cookie: {
+        secure: true,
+        maxAge: 100000
+    }
 }));
 
-// app.options('*', cors())
+app.use('/', userRouter);
 
-app.use('/',userRouter)
-
-app.listen(PORT,()=>{
-  console.log(`listening ${PORT}`)
-  
-})
+app.listen(PORT, () => {
+    console.log(`listening ${PORT}`);
+});
